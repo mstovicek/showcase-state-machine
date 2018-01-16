01a8a73b39d706c0075b1598e6a86fb74b3819d6
Milan Stovicek <stovicek.milan@gmail.com>
2018-01-16 14:38:02 +0100

    adds unit StateMachineTest

---
+++ tests/unit/StateMachineTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace StateMachine\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use StateMachine\Exception\StateNotAllowed;
+use StateMachine\StateMachine;
+
+class StateMachineTest extends TestCase
+{
+    public function testAllowStates()
+    {
+        $stateMachine = new StateMachine();
+
+        try {
+            $this->assertFalse(
+                $stateMachine->canTransit('a', 'b')
+            );
+            $this->fail('Transition between not allowed states should throw exception');
+        } catch (StateNotAllowed $e) {
+            // ok
+        }
+
+        $stateMachine->allowStates(['a', 'b']);
+
+        $this->assertFalse(
+            $stateMachine->canTransit('a', 'b')
+        );
+    }
+
+    public function testAllowTransitionCanTransit()
+    {
+        $stateMachine = new StateMachine();
+        $stateMachine->allowStates(['a', 'b', 'c']);
+
+        $this->assertFalse($stateMachine->canTransit('a', 'b'));
+        $this->assertFalse($stateMachine->canTransit('b', 'c'));
+        $this->assertFalse($stateMachine->canTransit('c', 'a'));
+
+        $stateMachine->allowTransition('a', 'b')
+            ->allowTransition('b', 'c');
+
+        $this->assertTrue($stateMachine->canTransit('a', 'b'));
+        $this->assertTrue($stateMachine->canTransit('b', 'c'));
+        $this->assertFalse($stateMachine->canTransit('c', 'a'));
+    }
+}
